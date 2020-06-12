c3b36503b8386ecd311214a238e4d1fe93b7f547
Abdelrahman Amer <36712666+chakertriangle@users.noreply.github.com>
2020-06-12 22:05:16 +0200

    last working version

--- queue_job.sh
+++ queue_job.sh
@@ -5,16 +5,16 @@ exec 1>/output/stdout.log 2>/output/stderr.log
 # TODO: Create MODEL variable
 # TODO: Create DEVICE variable
 # TODO: Create VIDEO variable
-MODELPATH=$1
-MODEL=$2
-DEVICE=$3
-VIDEO=$4
-QUEUE=$5
-OUTPUT=$6
+#MODELPATH=$1
+MODEL=$1
+DEVICE=$2
+VIDEO=$3
+QUEUE=$4
+OUTPUT=$5
 # TODO: Create PEOPLE variable
-PEOPLE=$7
+PEOPLE=$6
 
-mkdir -p $6
+mkdir -p $5
 
 if echo "$DEVICE" | grep -q "FPGA"; then # if device passed in is FPGA, load bitstream to program FPGA
     #Environment variables and compilation for edge compute nodes with FPGAs
